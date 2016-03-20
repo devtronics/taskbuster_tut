f04bbfcc7f628fe67c2ede50c2909ae96b467218
chrisheckler <hecklerchris@hotmail.com>
2016-03-20 01:23:38 -0700

    added new test, modified urls.py, modified views.py

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import home
+from .views import home, home_files
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
 ]
